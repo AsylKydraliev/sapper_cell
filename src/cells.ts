571dabcd8c39842bc2ba258ebf33f5873bc88a6b
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2021-11-09 16:51:49 +0600

    Реализован блок управления игрой, кнопка Reset,обновляет игру и метод подчсета попыток по нахождению крестика.

--- src/cells.ts
+++ src/cells.ts
@@ -4,12 +4,12 @@ export class Cell {
 
 export class CellBoard {
   cellsArray: Cell[] = [];
-  randomSymbol = Math.floor(Math.random() * 36 +1);
+  randomSymbol = Math.floor(Math.random() * 36);
 
   constructor() {
     for(let i = 0; i <= 35; i++){
       if(i === this.randomSymbol){
-        this.cellsArray.push(new Cell('', 'o'));
+        this.cellsArray.push(new Cell('', 'X'));
       } else {
         this.cellsArray.push(new Cell('', ''));
       }
